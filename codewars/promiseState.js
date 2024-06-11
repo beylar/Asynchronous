7886504ba461eb7df7cccc4e8dac7bb2d40d99be
Beyla Irakoze <beylaruz@gmail.com>
2024-06-11 17:17:36 +0200

    output the 3 promise states

---
+++ codewars/promiseState.js
@@ -0,0 +1,31 @@
+//using async and await
+async function getState(promise) {
+    try {
+        await promise;
+        return "fulfilled";
+    } catch (error) {
+        return "rejected"; 
+    }
+}
+
+
+function getState(promise) {
+    const pending = new Promise(resolve => setTimeout(resolve, 0, "pending"));
+    return Promise.race([promise, pending])
+        .then(result => {
+            if (result === "pending") {
+                return "pending";
+            }
+            return "fulfilled";
+        })
+        .catch(() => "rejected");
+}
+
+const p = Promise.resolve();
+getState(p).then(state => console.log(state)); // "fulfilled"
+
+const n = Promise.reject();
+getState(n).then(state => console.log(state)); // "rejected"
+
+const s = new Promise(() => {});
+getState(s).then(state => console.log(state)); // "pending"
