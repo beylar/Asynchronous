683769a88515431922e6a8c1c8d1268f97c6ec1d
Beyla Irakoze <beylaruz@gmail.com>
2024-06-16 14:52:45 +0200

    reject message to the entered id

--- codewars/promise.js
+++ codewars/promise.js
@@ -1,7 +1,13 @@
 function promiseHelloWorld() {
     // replace this nonsense with your Promise
-    return new Promise(function (resolve){
+    return new Promise(function (resolve,reject){
         // let value = 'Hello World!'
     // resolve(value);
-    resolve('Hello World!')
+    if(isNaN(id)){
+        resolve('Hello World!')
+
+    }
+    else{
+        reject("failed")
+    }
   })};
