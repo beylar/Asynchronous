74bbc98e90c41d2d5ba9704883f1b08076a2c493
Beyla Irakoze <beylaruz@gmail.com>
2024-06-17 18:55:36 +0200

    leveraging async and await with a promise

--- codewars/promise.js
+++ codewars/promise.js
@@ -1,13 +1,24 @@
-function promiseHelloWorld() {
-    // replace this nonsense with your Promise
-    return new Promise(function (resolve,reject){
-        // let value = 'Hello World!'
-    // resolve(value);
-    if(isNaN(id)){
-        resolve('Hello World!')
-
+async function promiseHelloWorld(str) {
+    // Convert the synchronous logic into an asynchronous operation
+    const result = await new Promise((resolve, reject) => {
+        try {
+            if (typeof str === 'string') {
+                resolve("valid value");
+            } else {
+                reject("Please enter a valid value");
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+    return result;
+}
+// console.log(promiseHelloWorld("yes"))
+(async () => {
+    try {
+        const message = await promiseHelloWorld("yes");
+        console.log(message);
+    } catch (error) {
+        console.error(error);
     }
-    else{
-        reject("failed")
-    }
-  })};
+})();
