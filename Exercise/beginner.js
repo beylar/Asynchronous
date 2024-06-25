04306f6110e08a55de31f151b1803285c486f6f1
Beyla Irakoze <beylaruz@gmail.com>
2024-06-25 17:05:38 +0200

    leveraging promise.all

--- Exercise/beginner.js
+++ Exercise/beginner.js
@@ -23,15 +23,38 @@
 // usingSetInterval()
 
 
-function chaining(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("My program is running!")
-            resolve()
-        })
-    })
+// function chaining(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve()
+//             return "My program is running!"
+//         })
+//     })
+// }
+// chaining()
+// .then(data => console.log("The data is as follows " + data))
+// .then(() => console.log("then the second promise"))
+// .catch(err => console.log("The error message"))
+
+
+async function asynchronousTest() {
+    const promise1 = new Promise((resolve, reject) => {
+        console.log("The first promise");
+        resolve()
+    });
+
+    const promise2 = new Promise((resolve, reject) => {
+        console.log("The second promise");
+        resolve();
+    });
+
+    const promise3 = new Promise((resolve, reject) => {
+        console.log("The third promise");
+        resolve();
+    });
+    await Promise.all([promise1, promise2, promise3]);
+
+    console.log("All promises resolved");
 }
-chaining()
-.then(data => console.log("The data is as follows " + data))
-.then(() => console.log("then the second promise"))
-.catch(err => console.log("The error message"))
+
+asynchronousTest();
