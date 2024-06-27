7cbcda5cafe4ee1674da40cf1d82e550ebf20cc3
Beyla Irakoze <beylaruz@gmail.com>
2024-06-27 11:24:57 +0200

    using the promise constructor method too attach thenables

--- Exercise/exercise.js
+++ Exercise/exercise.js
@@ -22,3 +22,13 @@ fn().then((res) => {
     console.log(res);
 });
 console.log('end')
+
+//exercise 3
+console.log("start");
+Promise.resolve('In Promise 1').then((res) => {
+    console.log(res);
+})
+Promise.resolve('In promise 2').then((res) => {
+    console.log(res);
+})
+console.log('end')
