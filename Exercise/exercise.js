8744839e9389948eec81b9ee98ce90982c86a3e9
Beyla Irakoze <beylaruz@gmail.com>
2024-06-27 11:23:28 +0200

    function expression and chaining a then() method on it

--- Exercise/exercise.js
+++ Exercise/exercise.js
@@ -1,3 +1,4 @@
+// exercise 1
 console.log('start')
 const promise = new Promise((resolve, reject) => {
     console.log('In promise');
@@ -5,5 +6,19 @@ const promise = new Promise((resolve, reject) => {
 }).then(res => {
     console.log('still in promise')
 })
+console.log('end')
 
+//exercise 2
+console.log("start");
+const fn = () =>
+    new Promise((resolve, reject) => {
+        console.log("In promise");
+        resolve("success");
+    });
+
+console.log("middle");
+
+fn().then((res) => {
+    console.log(res);
+});
 console.log('end')
